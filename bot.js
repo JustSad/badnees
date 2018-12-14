89d6389795475340f27138eec280603a2c52f0f9
JustSad <44894900+JustSad@users.noreply.github.com>
2018-12-14 14:03:38 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`ٍسقوط الانسان ليس فشل..ولكن الفشل. هو..أن يبقي حينما سقطٍ`,"http://twitch.tv/éMyyy")
+client.user.setGame(`ٍ ٍسقوط الانسان ليس فشل ولكن الفشل. هوا ان يبقي حينما سقطٍ`,"http://twitch.tv/éMyyy")
 client.user.setStatus("dnd")
 });
 
